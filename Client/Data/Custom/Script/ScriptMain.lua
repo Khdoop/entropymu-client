a6265585979978654e1dbfdc38ec19349f5b2c9f
Yordan Betsinski <yordan.betsinski@gmail.com>
2025-12-03 23:10:30 +0200

    december v1

---
+++ Client/Data/Custom/Script/ScriptMain.lua
@@ -0,0 +1,15 @@
+--############################################################################
+-- You need to set all scripts requires here
+-- ===========================================================================
+
+require('System\\ScriptCore')
+require('System\\ProtocolCore')
+require('System\\LoadImages')
+require('Util\\Defines')
+require('Util\\KeysValue')
+require('Util\\Json')
+
+-- requirefolder('Scripts\\TesteProtocol')		-- SAMPLE
+requirefolder('Scripts\\WindowTitle')		-- SAMPLE
+-- requirefolder('Scripts\\NewWindow')			-- SAMPLE
+requirefolder('Scripts\\QuestSystemNPC')
